b7f773aeb98aeb7411bbb683021ed6bf636a2036
hpc <philipchanhk626@gmail.com>
2020-04-08 09:29:10 -0400

    should find alternative for corsanywhere but basically done

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { Progress } from 'antd';
+import { Progress, Tag } from 'antd';
 
 function ProgressStats(props) {
-  
 
-
-
+  function getAverageTime(arr) {
+    let totalTime = 0;
+    let count = 0;
+    console.log(arr)
+    if (arr.length !== 0) {
+      for (const question of arr) {
+        for (const attempt in question.attempts) {
+          totalTime += question.attempts[attempt].time;
+          count++
+        }
+      }
+      return Math.round(totalTime / count);
+    } else {
+      return 0;
+    }
+  };
 
   return (
     <div className="progress-container">
       <div className="progress widget">
         <div className="progress__total">
-          <Progress 
-            type="circle" 
-            width={170} 
-            percent={(props.userProblems.length/props.problems.length)}
-            format={percent => `${props.userProblems.length} / ${props.problems.length}`} 
+          <Progress
+            type="circle"
+            width={170}
+            percent={props.userProblems.length / props.problems.length}
+            format={percent => `${props.userProblems.length} / ${props.problems.length}`}
           />
         </div>
         <div className="progress__breakdown">
@@ -29,7 +42,6 @@ function ProgressStats(props) {
             }}
           />
           <Progress
-            percent={30}
             percent={props.userMed.length / props.med.length}
             format={percent => `${props.userMed.length} / ${props.med.length}`}
             strokeColor={{
@@ -38,7 +50,6 @@ function ProgressStats(props) {
             }}
           />
           <Progress
-            percent={30}
             percent={props.userHard.length / props.hard.length}
             format={percent => `${props.userHard.length} / ${props.hard.length}`}
             strokeColor={{
@@ -48,8 +59,30 @@ function ProgressStats(props) {
           />
         </div>
         <div className="progress__averages">
-          Average
-      </div>
+          <div className="progress__averages--header">
+            Average Time Spent
+          </div>
+          <div className="progress__averages--grid">
+            <div>
+              <Tag color="green">Easy</Tag>
+            </div>
+            <div className="progress__averages--easy">
+              {`${getAverageTime(props.userEasy)} minutes`}
+            </div>
+            <div>
+              <Tag color="gold">Medium</Tag>
+            </div>
+            <div className="progress__averages--medium">
+              {`${getAverageTime(props.userMed)} minutes`}
+            </div>
+            <div>
+              <Tag color="red">Hard</Tag>
+            </div>
+            <div className="progress__averages--hard">
+              {`${getAverageTime(props.userHard)} minutes`}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
@@ -60,26 +93,26 @@ const mapStateToProps = state => {
   let easy = [];
   let med = [];
   let hard = [];
-  for (const p in all_problems) {
-    if (all_problems[p].difficulty.level === 1) {
-      easy.push(all_problems[p]);
-    } else if (all_problems[p].difficulty.level === 2) {
-      med.push(all_problems[p]);
-    } else if (all_problems[p].difficulty.level === 3) {
-      hard.push(all_problems[p]);
+  for (const p of all_problems) {
+    if (p.difficulty.level === 1) {
+      easy.push(p);
+    } else if (p.difficulty.level === 2) {
+      med.push(p);
+    } else if (p.difficulty.level === 3) {
+      hard.push(p);
     }
   };
 
   let userEasy = [];
   let userMed = [];
   let userHard = [];
-  for (const q in state.userProblems.problems) {
-    if (state.userProblems.problems[q].difficulty === 1) {
-      userEasy.push(state.userProblems.problems[q]);
-    } else if (state.userProblems.problems[q].difficulty === 2) {
-      userMed.push(state.userProblems.problems[q]);
-    } else if (state.userProblems.problems[q].difficulty === 3) {
-      userHard.push(state.userProblems.problems[q]);
+  for (const q of state.userProblems.problems) {
+    if (q.difficulty === 1) {
+      userEasy.push(q);
+    } else if (q.difficulty === 2) {
+      userMed.push(q);
+    } else if (q.difficulty === 3) {
+      userHard.push(q);
     }
   };
 
